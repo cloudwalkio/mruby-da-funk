eca946588cd37ab6d9fbaeccc19d72c7605ee651
Thiago Scalone <thiago@scalone.com.br>
2018-08-06 17:57:32 -0300

    Add documentation to Touch.rb

--- lib/src/touch.c
+++ lib/src/touch.c
@@ -7,26 +7,35 @@
 #include "mruby/string.h"
 #include "mruby/hash.h"
 
-/*#include "gedi.h"*/
-
 static mrb_value
 mrb_touch_s__getxy(mrb_state *mrb, mrb_value self)
 {
   /*mrb_int x = 0, y = 0, ret = 0, timeout = 0;*/
-  /*GEDI_LCD_e_TouchEvent event;*/
   mrb_value hash;
 
   /*mrb_get_args(mrb, "i", &timeout);*/
 
-  /*event:
-   *
-   *0	GEDI_LCD_TOUCHEVENT_NONE	No event detected
-   *1	GEDI_LCD_TOUCHEVENT_DOWN	Screen touched
-   *2	GEDI_LCD_TOUCHEVENT_MOVE	Movement while touching screen
-   *3	GEDI_LCD_TOUCHEVENT_UP	Screen no longer being touched
-   */
+  /*ret = TouchGet(&x, &y, &event, timeout, TRUE);*/
 
-  /*ret = GEDI_LCD_TouchGet(&x, &y, &event, timeout, TRUE);*/
+  hash = mrb_hash_new(mrb);
+  /*if ((ret == 0) && (event == 1)) {*/
+    /*mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "x")      , mrb_fixnum_value(x));*/
+    /*mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "y")      , mrb_fixnum_value(y));*/
+    /*mrb_hash_set(mrb , hash , mrb_str_new_lit(mrb , "return") , mrb_fixnum_value(ret));*/
+  /*}*/
+
+  return hash;
+}
+
+static mrb_value
+mrb_touch_s__getxy_stream(mrb_state *mrb, mrb_value self)
+{
+  /*mrb_int x = 0, y = 0, ret = 0, timeout = 0;*/
+  mrb_value hash;
+
+  /*mrb_get_args(mrb, "i", &timeout);*/
+
+  /*ret = TouchGet(&x, &y, &event, timeout, TRUE);*/
 
   hash = mrb_hash_new(mrb);
   /*if ((ret == 0) && (event == 1)) {*/
@@ -47,4 +56,5 @@ mrb_touch_init(mrb_state* mrb)
   touch    = mrb_define_class_under(mrb, platform, "Touch", mrb->object_class);
 
   mrb_define_class_method(mrb , touch , "_getxy" , mrb_touch_s__getxy , MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb , touch , "_getxy_stream" , mrb_touch_s__getxy , MRB_ARGS_REQ(1));
 }
--- mrblib/touch.rb
+++ mrblib/touch.rb
@@ -1,7 +1,27 @@
 class Platform
   class Touch
+    # Get coodinates X and Y of the touch event
+    #   This functions clean the queue before read
+    #
+    # @param timeout [Integer] Text to be printed.
+    # @return [NilClass] nil if no event was detected and timeout expired.
+    # @return [Array] [123, 25] ([X, Y]).
     def self.getxy(timeout = 10_000)
       hash = self._getxy(timeout)
+      if hash["return"] == 1
+        [hash["x"], hash["y"]]
+      end
+    end
+
+    # Get coodinates X and Y of the touch event
+    #   This functions don't clean the queue before read, so an event before
+    #   this executions could be detected.
+    #
+    # @param timeout [Integer] Text to be printed.
+    # @return [NilClass] nil if no event was detected and timeout expired.
+    # @return [Array] [123, 25] ([X, Y]).
+    def self.getxy_stream(timeout = 10_000)
+      hash = self._getxy_stream(timeout)
       if hash["return"] == 1
         [hash["x"], hash["y"]]
       end
